bc4910fea47fba429488e56f60ab7c9ba2679702
Serhii Klunniy <s.klunniy@gmail.com>
2024-04-24 03:45:17 -0400

    add edits 102

--- src/main/java/main/model/Parrot.java
+++ src/main/java/main/model/Parrot.java
@@ -2,12 +2,14 @@ package main.model;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Component(value = "p")
+@Primary
 public class Parrot {
 
     String name;
